1a44dafa789ba482ba557cc55b2343da9ece188d
chonky1234 <118490337+chonky1234@users.noreply.github.com>
2024-06-01 13:58:06 +0200

    get i set

--- biblioteka/Covek.cs
+++ biblioteka/Covek.cs
@@ -8,5 +8,69 @@ namespace Bibiloteka
     public class Covek : ICovek
     {
 
+        public string ID;
+
+        string status;
+        string Status
+        {
+            get 
+            { 
+                return status; 
+            }
+            set 
+            {
+                if (value == "Aktivan" || value == "Neaktivan")
+                {
+                    status = value;
+                }          
+            }
+        }
+
+        public string Ime;
+        public string Prezime;
+
+        string pol;
+        string Pol
+        {
+            get
+            {
+                return pol;
+            }
+            set
+            {
+                if (value == "Muski" || value == "Zenski")
+                {
+                    pol = value;    
+                }
+            }
+
+        }
+        public int GodinaRodjenja;
+        public List<string> Naponena = new List<string>();
+
+        public Covek(string id, string status, string ime, string prezime, string pol, int godinarodjenja, List<string> napomena)
+        {
+            this.ID = id;
+            this.Status = status;
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.Pol = pol;
+            this.GodinaRodjenja = godinarodjenja;
+            this.Naponena = napomena;
+        }
+
+        public void Dodavanje(string ID, string status, string ime, string prezime, string pol, int godina_rodjenja, List<Napomena> napomena)
+        {
+
+        }
+        public void IspravljanjePodataka(int ID)
+        {
+
+        }
+        public void PromenaStatusa()
+        {
+
+        }
+
     }
 }
